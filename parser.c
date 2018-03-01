ed0fb3663e6f5dbbc58bce113f0c4bd96e8f58ea
Mansour <melsharawy@stuy.edu>
2018-03-01 15:23:33 -0500

    done?

--- parser.c
+++ parser.c
@@ -60,6 +60,20 @@ void parse_file ( char * filename,
   FILE *f;
   char line[256];
   clear_screen(s);
+  int argstatus = 0;
+  color c;
+  c.red = MAX_COLOR;
+  c.green = MAX_COLOR;
+  c.blue = 0;
+
+  /*
+0 is null
+1 is save
+2 is line
+3 is move
+4 is scale
+5 is rotate
+  */
 
   if ( strcmp(filename, "stdin") == 0 ) 
     f = stdin;
@@ -69,6 +83,86 @@ void parse_file ( char * filename,
   while ( fgets(line, 255, f) != NULL ) {
     line[strlen(line)-1]='\0';
     printf(":%s:\n",line);
+    //print_matrix(edges);
+    if(!strcmp(line,"ident")){
+      ident(transform);
+    }
+    else if(!strcmp(line,"apply")){
+      matrix_mult(transform,edges);
+    }
+    else if(!strcmp(line,"display")){
+      clear_screen(s);
+      draw_lines(edges,s,c);
+      display(s);
+    }
+    else if(!strcmp(line,"save")){
+      argstatus = 1;
+    }
+    else if(!strcmp(line,"line")){
+      argstatus = 2;
+    }
+    else if(!strcmp(line,"move")){
+      argstatus = 3;
+    }
+    else if(!strcmp(line,"scale")){
+      argstatus = 4;
+    }
+    else if(!strcmp(line,"rotate")){
+      argstatus = 5;
+    }
+    else{
+      char * line2 = (char*)malloc(200);
+      char * line3 = strcpy(line2, line);
+      char ** arr = (char**)malloc(6 * sizeof(char*));
+      int i = 0;
+      while(line3){
+	arr[i] = strsep(&line3," ");
+	i++;
+      }
+      arr[i] = NULL;
+      if(argstatus == 1){
+	save_extension(s,line);
+      }
+      else if(argstatus == 2){
+	add_edge(edges,
+		 atof(arr[0]),
+		 atof(arr[1]),
+		 atof(arr[2]),
+		 atof(arr[3]),
+		 atof(arr[4]),
+		 atof(arr[5])
+		 );
+      }      
+      else if(argstatus == 3){
+	struct matrix * m = make_translate(
+					   atof(arr[0]),
+					   atof(arr[1]),
+					   atof(arr[2]));
+	matrix_mult(m,transform);
+      }
+      else if(argstatus == 4){
+	struct matrix * m = make_scale(
+				       atof(arr[0]),
+				       atof(arr[1]),
+				       atof( arr[2]));
+	matrix_mult(m,transform);
+      }
+      else if(argstatus == 5){
+	if(!strcmp(arr[0],"x")){
+	  struct matrix * m = make_rotX(M_PI*atof(arr[1])/180);
+	  matrix_mult(m,transform);
+	}
+	else if(!strcmp(arr[0],"y")){
+	  struct matrix * m = make_rotY(M_PI*atof(arr[1])/180);
+	  matrix_mult(m,transform);
+	}
+	else{
+	  struct matrix * m = make_rotZ(M_PI*atof(arr[1])/180);
+	  matrix_mult(m,transform);
+	}
+ 
+      }
+    }
   }
 }
   
